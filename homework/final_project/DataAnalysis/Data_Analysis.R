737ffa8f4f58e207350fa7ad20ec1a20480cb4e3
Haley Dylewski <hdylewsk@vols.utk.edu>
2020-12-03 10:59:57 -0500

    Running app with tabs

--- homework/final_project/DataAnalysis/Data_Analysis.R
+++ homework/final_project/DataAnalysis/Data_Analysis.R
@@ -9,6 +9,12 @@
 
 library(shiny)
 library(shinydashboard)
+library(shinythemes)
+library(tidyverse)
+library(ggplot2)
+library(viridis)
+library(emmeans)
+library(rstatix)
 
 
 
@@ -92,7 +98,11 @@ ui <-dashboardPage(
                                    submitButton("Update")
                                )
                         ),
-                        fluidRow()# for output tables etc
+                        fluidRow(
+                          column(7,offset = 0,
+                                 h4("Summary statistics", align = "center"),
+                                 tableOutput("sum_stats")                                 )
+                        )# for output tables etc
                         
                     )#close fluidpage
             ),# close second tab
@@ -188,14 +198,311 @@ server <- function(input, output) {
         req(data)
         validate(need(ext == "csv", "Please confirm uploaded file extension is saved
                   in a '.csv' format."))
-        read.csv(data$datapath, header = input$header, check.names = FALSE)
-    })
+        data_1 <- read.csv(data$datapath, header = input$header, check.names = FALSE)
+        
+        ### This 'for loop' ensures that the user is only inputting the appropriate
+        ### information type
+
+        #  for (i in colnames(data_1)){
+        #   data_1_columns <- c(colnames(data_1))
+        #   if (i == data_1_columns[1]){
+        #     next
+        #   }
+        #   else if (class(data_1[[i]]) != "numeric"){
+        #     if(class(data_1[[i]]) != "integer"){
+        #       validate(need(class(data_1[[i]]) == "numeric", "A cell that is
+        #               neither a row nor column header holds a non-numeric value.
+        #               Please ensure ALL non-row header values are numeric."))
+        #     }
+        #   }
+        #   # else if (sum(is.na(as.numeric(data_1_columns))) != 1) {
+        #   #   validate(need(sum(is.na(as.numeric(data_1_columns))) == '[1] 1', "At least
+        #   #             one column header is not a numeric value. Please ensure all column
+        #   #             headers are numeric."))
+        #   # }
+        #   else{
+        #
+        #   }
+        # }
+      })#close sample_data
     
     
+  #__________________ Output table that shows user data input __________  
     output$csv.data <- renderTable({
         data <- sample_data()
     })
+   
     
+    
+  # # __________________ Statistical Analysis _____________________________  
+
+    ################### Create Output   ###################
+
+    ### reactive list to store results of the statistical analysis
+   stat_results <- reactiveValues()
+
+    ## output
+   output$sum_stats <- renderTable({
+     data <- stat_results$sum_stats
+   })
+
+   ################### Do computation   ###################
+   ## 1) Data is formatted, characters are converted to factors
+   ## 2) Summary statistics are generated and outliers identified
+   ## 3) Analysis methods assume that data is normal with homogeneous variance.
+   ##    Normality is assessed using the Shapiro-Wilks test. For few groups with
+   ##    many data points (>20), normality of each group individually is assessed.
+   ##    For many groups with few data points (<20), normality of all groups
+   ##    together is assessed by analyzing model (Sample vs Time points) residuals.
+   ##    Homoegeneity of variances is asseed by levene test.
+   ##    A p- value <0.05 indicates failure of these tests.
+   ##    If these tests fail, the statisical analysis will still run but a
+   ##    warning will issue
+   ## 4) Statistical analysis is performed based on user choice
+   ##### a) No stats
+   ##### b) T-Test: pairwise students T-test with bonferoni p-value adjustment
+   ##### c) One-way ANOVA: Different types of ANOVA are carried out based on the results
+   #####    of the levene test for homogeneity of variances
+   #####       -> Passed: One-way ANOVA is done. If and significant interactions
+   #####         were found, pairwise comparisons are done using Tukey's method
+   #####       -> Failed: A Welch ANOVA is performed. If significant, Games-Howell
+   #####         post-hoc test is performed
+   ##### d) Two-way ANOVA: A two-way ANOVA is carried out using Sample and Treatment
+   #####    if data passed the Levene test. Otherwise one way ANOVAS are suggested
+   #####       -> if significant, post hoc pairwise comparison is computed using
+   #####         emmeans_test with bonferroni p-value adjustment, and simple main
+   #####         effects are computed by one-way ANOVA on data grouped by Sample
+   #####       -> if not significant, pairwise comparisons are carried out using
+   #####         emmeans test with error based on model
+   #####
+
+   ## output variables:
+   ## sum_stats = summary statistics including averages, standard deviation, error
+   ## outliers = outlier values
+   ## shapiro_result = TRUE/FALSE Normality of data
+   ## levene_result = TRUE/FALSE Homogeneity of variance
+   ## stat.test = anova result
+   ## pwc = pairwise comparisons post hoc or t-test
+   ## significance = TRUE/FALSE if any significance was found in ANOVA
+
+
+   observe({
+
+     ### Format data and do summary stats.
+     ### Add the results to the stat_results list
+     if (input$user_stat_choice == "2 Way ANOVA"){
+       long_data_stats <- pivot_longer(sample_data(),
+                                       cols = !contains('Sample') & !contains("Treatment"),
+                                       names_to = "Time_points",
+                                       values_to = "Number")
+
+       data_set <- mutate_if(long_data_stats, is.character, as.factor)
+       groupings <- groupings <- c("Sample", "Treatment")## this allows groupings to be set by user if we so desir later rather than hard coded
+
+       outliers <-  data_set %>%
+         group_by(groupings[1], groupings[2])%>%
+         identify_outliers(Number)
+       sum_stats <- data_set %>%
+         group_by(groupings[1], groupings[2]) %>%
+         get_summary_stats(Number, type = "mean_sd")
+      stat_results$sum_stats <- sum_stats
+
+     }else {
+       long_data_stats <- pivot_longer(sample_data(),
+                                       cols = !contains('Sample'),
+                                       names_to = "Time_points",
+                                       values_to = "Number")
+
+      data_set <- mutate_if(long_data_stats, is.character, as.factor)
+
+       outliers <-  data_set %>%
+         group_by(Time_points,Sample)%>%
+         identify_outliers(Number)
+       sum_stats <- data_set %>%
+         group_by(Time_points,Sample) %>%
+         get_summary_stats(Number, type = "mean_sd")
+       stat_results$sum_stats <- sum_stats
+     }
+
+
+
+     ###  --------- Check normality assumptions  ---------
+     data_set_length <- sum_stats$n[1]
+
+     if (input$user_stat_choice == "2 Way ANOVA"){ # two ways
+       if (data_set_length <= 20){
+         model <- lm(Number ~ Sample*Treatment, data = data_set)
+         shapiro <- shapiro_test(residuals(model))
+         scheck <- shapiro$p.value
+         for (i in seq_along(scheck)){
+           if (scheck[i] >0.05){shapiro_result = TRUE
+           }else {
+             shapiro_result = FALSE
+             break
+           }
+         }
+       }else{
+         group_norm <- data_set%>%
+           group_by(Sample,Treatment) %>%
+           shapiro_test(Number)
+         gncheck <- group_norm$p
+         for (i in seq_along(gncheck)){
+           if (gncheck[1] >0.05){shapiro_result = TRUE
+           }else {shapiro_result = FALSE
+           break
+           }
+         }
+       }
+       levene <- data_set %>%
+         levene_test(Number ~ Sample*Treatment)
+       levcheck <- levene$p
+       for (i in seq_along(levcheck)){
+         if (levcheck[1] >0.05){lev_result = TRUE
+         }else {lev_result = FALSE
+         break
+         }
+       }
+
+
+     }else{ ##one ways
+       if (data_set_length >= 20){
+         group_norm <- data_set%>%
+           group_by(Sample) %>%
+           shapiro_test(Number)
+         gncheck <- group_norm$p
+         for (i in seq_along(gncheck)){
+           if (gncheck[i] >0.05){shapiro_result = TRUE
+           }else {
+             shapiro_result = FALSE
+             break
+           }
+         }
+       }else {
+         model <- lm(Number ~ Sample, data = data_set)
+         shapiro <- shapiro_test(residuals(model))
+         scheck <- shapiro$p.value
+         for (i in seq_along(scheck)){
+           if (scheck[i] >0.05){shapiro_result = TRUE
+           }else {
+             shapiro_result = FALSE
+             break
+           }
+         }
+       }
+       levene <- data_set%>%
+         levene_test(Number ~ Sample)
+       levcheck <- levene$p
+       for (i in seq_along(levcheck)){
+         if (levcheck[1] >0.05){lev_result = TRUE
+         }else {lev_result = FALSE
+         break
+         }
+       }
+     }
+
+     ### --------- Perform statistical analysis  ---------
+
+     ### a function to call later that checks for significance after the
+     ### test is run
+     significance_check <- function(x){
+       sig = FALSE
+       for (i in seq_along(x)){
+         if(x[i] == '*'){
+           sig = TRUE
+         }
+       }
+       return(sig)
+     }
+
+     ### The value for significance is initially set to FALSE to prevent
+     ### any accidental carryover from previous calculations
+     significance = FALSE
+
+     ## Do T-test or ANOVA
+
+     if(input$user_stat_choice == "None"){
+       print("No statistical analysis preformed")
+
+     }else if(input$user_stat_choice == "T-Test"){
+       print("Pairwise student's T-test with p-value adjusted using Bonferroni method")
+       stat.test <- data_set%>%
+         group_by(Time_points)%>%
+         pairwise_t_test(
+           Number ~ Sample,
+           p.adjust.method = "bonferroni"
+         )
+       pwc <- stat.test
+     }else if(input$user_stat_choice == "One Way ANOVA"){
+
+       ## perform normal anova if levin is true
+       if(levene_result == TRUE){
+         stat.test <- data_set %>%
+           group_by(Time_points) %>%
+           anova_test(Number ~ Sample)
+         stat.test
+         ## determine if significant
+         stat_check <- stat.test[[7]]
+         stat_check
+         significance <- significance_check(stat_check)
+
+         ## post hoc given significant ANOVA
+         if (significance == TRUE){
+           ## Tukey comparisons
+           pwc <- data_set %>%
+             group_by(Time_points) %>%
+             tukey_hsd(Number ~ Sample)
+
+         } else { print("No significance")}
+
+         ## if levene test was failed, use Welch anova
+       }else if ( levene_result == FALSE){
+         stat.test <- data_set %>%
+           group_by(Time_points)%>%
+           welch_anova_test(Number ~ Sample)
+         ## determine if significant
+         stat_check <- stat.test[[7]]
+         significance <- significance_check(stat_check)
+
+         ## Games-Howell post hoc given significant ANOVA
+         if (significance == TRUE){
+           pwc <- data_set %>%
+             group_by(Time_points)%>%
+             games_howell_test(Number ~ Sample)
+         } else { print("No significance")}
+       }
+
+
+     }else if (input$user_stat_choice == "2 Way ANOVA") {
+       if(levene_result == TRUE){
+         stat.test <- data_set %>% anova_test(Number ~ Sample*Treatment)
+         stat_check <- stat.test[[6]]
+         significance <- significance_check(stat_check)
+         if (significance == TRUE){
+           ###pairwise comparisons
+           pwc <- data_set %>%
+             group_by(Sample)%>%
+             emmeans_test(Number ~ Treatment, p.adjust.method = "bonferroni")
+         }
+         ##simple main effects
+         model<- lm(Number ~ Sample*Treatment, data = data_set)
+         main_eff_Treat <- data_set %>%
+           group_by(Sample) %>%
+           anova_test(Number ~ Treatment, error = model)
+       }else if(significance == FALSE){
+         model <- lm(Number ~ Sample*Treatment, data = data_set)
+         pwc <- emmeans_test(Number ~ Treatment, p.adjust.method = "bonferoni", model = model)
+
+       }else if (levene_result == FALSE){
+         print("Data failed levene test for homogeneity of variances. Consider one-way ANOVAS")
+       }
+     }
+   })
+
+
+    
+    
+    
+    #__________________ Generate Plot __________________________ 
     
     ### Plot progress so far. The file is successfully connected to file upload.
     ### The 'data' and 'ext' features can likely be streamlined in the future,
@@ -216,49 +523,22 @@ server <- function(input, output) {
     ###    saving as 'Sample' it will save as 'i..Sample'. I tried adjusting the
     ###    code to account for this but got some errors.
     
+   
+
     output$csv.plot <- renderPlot({
-        data <- input$data_file
-        ext <- tools::file_ext(data$datapath)
-        
-        req(data)
-        validate(need(ext == "csv", "Please confirm uploaded file extension is saved
-                  in a '.csv' format."))
-        data_1 <- read.csv(data$datapath, header = input$header, check.names = FALSE)
-        
-        ### This 'for loop' ensures that the user is only inputting the appropriate
-        ### information type
-        for (i in colnames(data_1)){
-            data_1_columns <- c(colnames(data_1))
-            if (i == data_1_columns[1]){
-                next
-            }
-            else if (class(data_1[[i]]) != "numeric"){
-                if(class(data_1[[i]]) != "integer"){
-                    validate(need(class(data_1[[i]]) == "numeric", "A cell that is
-                      neither a row nor column header holds a non-numeric value.
-                      Please ensure ALL non-row header values are numeric."))
-                }
-            }
-            else if (sum(is.na(as.numeric(data_1_columns))) != 1) {
-                validate(need(sum(is.na(as.numeric(data_1_columns))) == '[1] 1', "At least 
-                      one column header is not a numeric value. Please ensure all column 
-                      headers are numeric."))
-            }
-            else
-                next
-        }
-        
-        long_data <- pivot_longer(data_1,
+      data <- sample_data()
+
+        long_data <- pivot_longer(sample_data(),
                                   cols = !contains('Sample'),
                                   names_to = "Time_Points",
                                   values_to = "Number"
         )
-        
-        ### ggplot takes many togglable options from the user 
+
+        ### ggplot takes many togglable options from the user
         ### this allows the user to format the axis titles , Graph title,
         ### aixs labels, and overall color scheme. All color schemes are cupposed to be color blind friendly
         long_data_final <- rename(long_data, 'Sample' = contains('Sample'))
-        
+
         ggplot(long_data_final, aes(x = reorder(Time_Points, sort(as.numeric(Time_Points))), y = Number, color = Sample, group = Sample))+
             geom_line(linetype = "solid") +
             geom_point() +
@@ -271,7 +551,7 @@ server <- function(input, output) {
                                             face = "bold", size = input$user_axis_size),
                   axis.text.x = element_text(face= "plain", color = "#666666" , size = input$user_xylabel_size))+ ### Axis label size, color is currently hard coded
             scale_color_viridis(discrete = TRUE, option = input$user_color_palette) ### sets a color blind friendly color pallette
-        
+
     })
 }#close server
 
